ab47869d4f9a6752c295c7f6c75a62a09f51c345
evrooije <evrooije@outlook.com>
2017-06-11 17:20:50 +0200

    Disabled important message for the server migration

--- mods/00_bt_messages/00_bt_important_messages/init.lua
+++ mods/00_bt_messages/00_bt_important_messages/init.lua
@@ -1,10 +1,7 @@
 local MESSAGE_INTERVAL = 300
 
 important_message = {}
-important_message.message =
-"IMPORTANT!! This server will migrate to bigger hardware in the next couple of days."..
-" There may be some intermittent outages and the migration will take about an hour as"..
-" all data will be copied over. The new server address will be beerarchy.tk:30024."
+important_message.message = ""
 
 function important_message.display_message()
 	if important_message.message and important_message.message ~= "" then
@@ -18,4 +15,6 @@ function important_message.start_spamming()
 	minetest.after(MESSAGE_INTERVAL, important_message.start_spamming)
 end
 
-important_message.start_spamming()
+if important_message.message and important_message.message ~= "" then
+	important_message.start_spamming()
+end
