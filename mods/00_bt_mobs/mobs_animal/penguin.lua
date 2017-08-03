216ccfd52cb0f0ef3c2f9135bc198fd6ab7cfb0d
evrooije <evrooije@outlook.com>
2017-08-03 22:45:03 +0200

    Fixed pengiuns to now follow fishing mod fish instead of ethereal

--- mods/00_bt_mobs/mobs_animal/penguin.lua
+++ mods/00_bt_mobs/mobs_animal/penguin.lua
@@ -44,7 +44,8 @@ mobs:register_mob("mobs_animal:penguin", {
 	},
 	fly_in = "default:water_source",
 	floats = 0,
-	follow = {"ethereal:fish_raw"},
+	follow = {"mobs_animal:rat", "fishing:bluewhite_raw", "fishing:carp_raw", "fishing:catfish_raw", "fishing:clownfish_raw", "fishing:exoticfish_raw",
+			  "fishing:fish_cooked", "fishing:fish_raw", "fishing:perch_raw", "fishing:pike_cooked", "fishing:pike_raw"},
 	view_range = 5,
 
 	on_rightclick = function(self, clicker)
